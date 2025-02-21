94939719ef494bac9a3235ce0aa2fb1a939c9c2b
Esteshhad <shsh362001@gmail.com>
2025-02-21 08:55:25 +0200

    Finished On Boarding and Applying Shared Preferences.

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,30 @@ import 'package:islami/app_theme.dart';
 import 'package:islami/hadeth/hadeth_details_screen.dart';
 import 'package:islami/home_screen.dart';
 import 'package:islami/models/sura_model.dart';
+import 'package:islami/onboarding/onboarding_screen.dart';
 import 'package:islami/quran/sura_details_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SuraModel.getMostRecentlyFromSharedPrefs();
-  runApp(MyApp());
+  bool first = await checkIsFirstTime();
+  runApp(MyApp(
+    firstTime: first,
+  ));
+}
+
+Future<bool> checkIsFirstTime() async {
+  final prefs = await SharedPreferences.getInstance();
+  bool isFirst = prefs.getBool("isFirstTime") ?? true;
+  return isFirst;
 }
 
 class MyApp extends StatelessWidget {
+  bool firstTime;
+
+  MyApp({required this.firstTime});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,9 +34,11 @@ class MyApp extends StatelessWidget {
       routes: {
         HomeScreen.routeName: (context) => HomeScreen(),
         SuraDetailsScreen.routeName: (context) => SuraDetailsScreen(),
-        HadethDetailsScreen.routeName: (context) => HadethDetailsScreen()
+        HadethDetailsScreen.routeName: (context) => HadethDetailsScreen(),
+        OnBoardingScreen.routeName: (context) => OnBoardingScreen()
       },
-      initialRoute: HomeScreen.routeName,
+      initialRoute:
+          firstTime ? OnBoardingScreen.routeName : HomeScreen.routeName,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.dark,
     );
---
+++ lib/models/page_view_element.dart
@@ -0,0 +1,8 @@
+class PageViewElement {
+  String image;
+  String title;
+  String? desc;
+
+  PageViewElement(
+      {required this.image, required this.title, required this.desc});
+}
